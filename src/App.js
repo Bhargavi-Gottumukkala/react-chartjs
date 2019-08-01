fb037aaec35b185c157284351d081ae2d076fe86
Bhargavi Gottumukkala <bhargavi.gottumukkala@senecaglobal.net>
2019-08-01 14:30:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Chart from './components/Chart';
+import CustomChart from './components/Chart';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      chartData: {}
-    }
-  }
-
-  componentWillMount() {
-    this.getChartData();
-  }
-
-
-  getChartData() {
-    const barData = [95, 110, 130, 150, 158, 169];
-    const colors = barData.map(value => (value <= 95) ? 'green' : (((value > 90) && (value < 140)) ? 'orange' : 'red'));
-    this.setState({
-      chartData: {
-        labels: ['data1', 'data2', 'data3', 'data4', 'data5', 'data6'],
-        datasets: [{
-          type: 'line',
-          label: 'Line Chart',
-          borderColor: 'gray',
-          borderWidth: 2,
-          fill: false,
-          data: [100, 120, 140, 150, 160, 170]
-        }, {
-          type: 'bar',
-          label: 'Bar Chart',
-          data: barData,
-          borderColor: 'white',
-          backgroundColor: colors,
-          borderWidth: 2
-        }
-        ]
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Chart chartData={this.state.chartData} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <CustomChart width={500} height={300} normal={'green'} warning={'orange'} critical={'red'}/>
+  )
 }
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
-function Chart(props) {
-    return <Bar data={props.chartData} width={1060} height={360} />
+function CustomChart(props) {
+    const barData = [95, 110, 130, 150, 158, 169];
+    const colors = barData.map(value => (value <= 95) ? props.normal : (((value > 90) && (value < 140)) ? props.warning : props.critical));
+    const chartData = {
+        labels: ['data1', 'data2', 'data3', 'data4', 'data5', 'data6'],
+        datasets: [{
+            type: 'line',
+            label: 'Line Chart',
+            borderColor: 'gray',
+            borderWidth: 2,
+            fill: false,
+            data: [100, 120, 140, 150, 160, 170]
+        }, {
+            type: 'bar',
+            label: 'Bar Chart',
+            data: barData,
+            borderColor: 'white',
+            backgroundColor: colors,
+            borderWidth: 2
+        }
+        ]
+    }
+    const options = {
+        maintainAspectRatio: false,
+        responsive: false
+    }
+    return (
+        <Bar data={chartData} options={options} width={props.width} height={props.height} />
+    )
 }
 
-export default Chart;
+export default CustomChart;
