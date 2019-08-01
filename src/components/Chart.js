23c9c4c9d8e944229a1b4ede20d93d24b2868e02
Bhargavi Gottumukkala <bhargavi.gottumukkala@senecaglobal.net>
2019-08-01 10:51:17 +0530

    mixed-chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,21 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Bar} from 'react-chartjs-2';
 
-class Chart extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      chartData:props.chartData
-    }
-  }
-
-  render(){
-    return (
-      <div className="chart">
-        <Bar data={this.state.chartData} width={1060} height={360} />
-      </div>
-    )
-  }
+function Chart(props) {
+    return <Bar data={props.chartData} width={1060} height={360} />
 }
 
 export default Chart;
